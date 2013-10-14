687eabc4f13d0540ab98ba465d47aa12f481d0e4
Alexander Ivanov <ttecss@gmail.com>
2013-10-14 20:21:27 +0300

    Instance watcher

--- present.js
+++ present.js
@@ -1,8 +1,18 @@
 (function(window){
 	var $ = window.jQuery;
+	var instance = false;
+	
+	var Present = function( options ) {
+		return this;
+	}
 	
 	$.fn.Present = function( options ) {
-		return this;
+		if(instance) {
+			if (window.console) window.console.warn('Present.js is already instantiated!');
+			return instance;
+		} else {
+			return instance = new Present( options );
+		}
 	}
 	
 })(window);
